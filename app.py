264e3d9e26404882c169b97c101d89605448ad20
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 02:25:47 +0300

    completed Lab assignment

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_uri'  # Replace with your database URI
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(100))
